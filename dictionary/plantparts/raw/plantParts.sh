791cdb714be524ced3d6b667f93998f668909128
Ambarish Kumar <36997739+ambarishK@users.noreply.github.com>
2019-10-14 14:49:03 +0530

    Script to prepare plant parts dictionary.

---
+++ dictionary/plantparts/raw/plantParts.sh
@@ -0,0 +1,3 @@
+
+
+ami-dictionary create --terms "aerial (flowering)"  "aerial (fruit maturing)"  "aerial parts"  "aerial  (before flowering )"  "branch"  "bud"  "Bulbs"  "floral buds"  "flower"  "fruit"  "inner bark"  "leaf"  "nuts"  "peel"  "rhizomes"  "ripe fruits"  "root"  "seed"  "seedling"  "seeds"  "stem"  "whole plants"  --dictionary plantParts --directory plantParts --outformats xml, html
